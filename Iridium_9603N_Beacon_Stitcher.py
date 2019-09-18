85da505e3d0e281db5cf5d4857a3b7754bfea76f
Patrick Kappl <kappl.pa@aon.at>
2019-09-18 05:20:52 +0200

    Add lat, lon, gnss_altitude data to .csv file

--- Iridium_9603N_Beacon_Stitcher.py
+++ Iridium_9603N_Beacon_Stitcher.py
@@ -4,6 +4,7 @@
 
 import os
 import numpy as np
+import struct
 
 try:
     momsns = np.loadtxt('SBD_headers_stitched.csv',
@@ -40,10 +41,19 @@ for root, dirs, files in os.walk("."):
                         fp.write(session_time.encode("utf-8"))
                         fp.write(msn.encode("utf-8"))
                         fr = open(longfilename, 'rb')
-                        fp.write(fr.read())
+                        binary_data = fr.read()
+                        timestamp = str(int.from_bytes(binary_data[1:5], "big")) + ","
+                        lat = str(struct.unpack("f", binary_data[5:9])[0]) + ","
+                        lon = str(struct.unpack("f", binary_data[9:13])[0]) + ","
+                        gnss_altitude = str(struct.unpack("f", binary_data[13:17])[0]) + ","
+                        fp.write(timestamp.encode("utf-8"))
+                        fp.write(lat.encode("utf-8"))
+                        fp.write(lon.encode("utf-8"))
+                        fp.write(gnss_altitude.encode("utf-8"))
                         fp.write('\r\n'.encode("utf-8"))
                     else:
-                        print('Skipping', longfilename, '(Could not match momsn)')
+                        print('Skipping', longfilename,
+                              '(Could not match momsn)')
                     fr.close()
 
 fp.close()
