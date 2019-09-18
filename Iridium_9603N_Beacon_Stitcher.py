cb52c6538d7d8ad4e2ac950938f20fface9ac2c0
Patrick Kappl <kappl.pa@aon.at>
2019-09-18 04:49:02 +0200

    Make .sbd file stitching work

--- Iridium_9603N_Beacon_Stitcher.py
+++ Iridium_9603N_Beacon_Stitcher.py
@@ -6,22 +6,24 @@ import os
 import numpy as np
 
 try:
-    momsns = np.loadtxt('SBD_headers_stitched.csv', delimiter=',', unpack=True, usecols=(1,))
-    session_times = np.genfromtxt('SBD_headers_stitched.csv', delimiter=',', unpack=True, usecols=(3,), dtype='str')
+    momsns = np.loadtxt('SBD_headers_stitched.csv',
+                        delimiter=',', unpack=True, usecols=(1,))
+    session_times = np.genfromtxt(
+        'SBD_headers_stitched.csv', delimiter=',', unpack=True, usecols=(3,), dtype='str')
 except:
     raise Exception('Could not process SBD_headers_stitched.csv!')
 
-fp = open('SBD_stitched.csv','wb')
-print 'Stitching sbd files into SBD_stitched.csv'
-#print 'Using momsns:'
-#print momsns
-#print 'Using session_times:'
-#print session_times
+fp = open('SBD_stitched.csv', 'wb')
+print('Stitching sbd files into SBD_stitched.csv')
+# print 'Using momsns:'
+# print momsns
+# print 'Using session_times:'
+# print session_times
 
 for root, dirs, files in os.walk("."):
     if len(files) > 0:
-        #if root != ".": # Only check sub-directories
-        if root == ".": # Only check this directory
+        # if root != ".": # Only check sub-directories
+        if root == ".":  # Only check this directory
             for filename in files:
                 if filename[-4:] == '.sbd':
                     longfilename = os.path.join(root, filename)
@@ -30,20 +32,18 @@ for root, dirs, files in os.walk("."):
                     msnum = float(longfilename[-10:-4])
                     session_time = ''
                     for index, seqnum in enumerate(momsns):
-                        if seqnum == msnum: session_time = session_times[index] + ','
+                        if seqnum == msnum:
+                            session_time = session_times[index] + ','
                     if session_time != '':
-                        print 'Appending',longfilename
-                        fp.write(rxtime)
-                        fp.write(session_time)
-                        fp.write(msn)
-                        fr = open(longfilename,'rb')
+                        print('Appending', longfilename)
+                        fp.write(rxtime.encode("utf-8"))
+                        fp.write(session_time.encode("utf-8"))
+                        fp.write(msn.encode("utf-8"))
+                        fr = open(longfilename, 'rb')
                         fp.write(fr.read())
-                        fp.write('\r\n')
+                        fp.write('\r\n'.encode("utf-8"))
                     else:
-                        print 'Skipping',longfilename,'(Could not match momsn)'
+                        print('Skipping', longfilename, '(Could not match momsn)')
                     fr.close()
-                
-fp.close()
 
-            
-    
+fp.close()
